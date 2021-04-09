95409ffd06ae7083d98de906176da6fb126c19f4
Mikhail356 <michael_gvozdev@mail.ru>
2021-04-09 09:38:33 +0300

    Update data in serv & some change in parse mf2

--- parsmf2.py
+++ parsmf2.py
@@ -3,8 +3,12 @@ import json
 import sys
 
 if len(sys.argv) < 2:
-    obj = mf2py.parse(url="http://tantek.com/2021/073/t8/balance-who-utility-resiliency/")
+    obj = mf2py.Parser(url="http://tantek.com/2021/073/t8/balance-who-utility-resiliency/")
 else:
-    obj = mf2py.parse(url=sys.argv[1])
+    obj = mf2py.Parser(url=sys.argv[1])
 with open('person.json', 'w') as json_file:
-  json.dump(obj, json_file, indent = 4, sort_keys = True)
+#  json.dump(obj, json_file, indent = 4, sort_keys = True)
+#    f = obj.to_json()  # returns a JSON string
+#    json_file.write(f)
+    f = obj.to_json(pretty_print = True)
+    json_file.write(f)
