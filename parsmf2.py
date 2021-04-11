213a2a7dba6244a6af1367fd15b89e0675a2594b
Mikhail356 <michael_gvozdev@mail.ru>
2021-04-11 09:40:00 +0300

    Improve html code of web pages

--- parsmf2.py
+++ parsmf2.py
@@ -3,12 +3,11 @@ import json
 import sys
 
 if len(sys.argv) < 2:
-    obj = mf2py.Parser(url="http://tantek.com/2021/073/t8/balance-who-utility-resiliency/")
+    obj = mf2py.parse(url="http://tantek.com/2021/073/t8/balance-who-utility-resiliency/")
 else:
-    obj = mf2py.Parser(url=sys.argv[1])
-with open('person.json', 'w') as json_file:
-#  json.dump(obj, json_file, indent = 4, sort_keys = True)
-#    f = obj.to_json()  # returns a JSON string
-#    json_file.write(f)
-    f = obj.to_json(pretty_print = True)
-    json_file.write(f)
+    obj = mf2py.parse(url=sys.argv[1])
+with open('out.json', 'w') as file:
+    json.dump(obj, file, indent = 4, sort_keys = False, ensure_ascii = False)
+#    obj = obj.encode('utf-8')
+#    f = obj.to_json(pretty_print = True)
+#    file.write(f)
