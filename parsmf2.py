07a3a9a8dd85c90a9c640f0248448289605b7aac
Mikhail356 <michael_gvozdev@mail.ru>
2021-04-12 21:25:35 +0300

    Improve code of web pages and parse

--- parsmf2.py
+++ parsmf2.py
@@ -6,8 +6,10 @@ if len(sys.argv) < 2:
     obj = mf2py.parse(url="http://tantek.com/2021/073/t8/balance-who-utility-resiliency/")
 else:
     obj = mf2py.parse(url=sys.argv[1])
-with open('out.json', 'w') as file:
-    json.dump(obj, file, indent = 4, sort_keys = False, ensure_ascii = False)
+print(json.dumps(obj, indent = 4, sort_keys = False, ensure_ascii = False))
+#with open('out.json', 'w') as file:
+#    json.dump(obj, file, indent = 4, sort_keys = False, ensure_ascii = False)
+#    print(json.dumps(obj, indent = 4, sort_keys = False, ensure_ascii = False))
 #    obj = obj.encode('utf-8')
 #    f = obj.to_json(pretty_print = True)
 #    file.write(f)
