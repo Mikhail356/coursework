3db39aeb0a28c5d831c24c1b37d83eab15b20e6c
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2021-05-25 16:51:46 +0300

    citations

--- cw/build.sh
+++ cw/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-pandoc -f gfm+tex_math_dollars -t latex -o _coursework_pandoc.tex <(cat intro.md tech.md math.md real.md end.md l_list.md)
+
+cat intro.md tech.md math.md real.md end.md l_list.md > _coursework_pandoc.md
+# https://github.com/citation-style-language/schema/blob/master/schemas/input/csl-data.json
+pandoc -f markdown_github+tex_math_dollars+citations -t latex -o _coursework_pandoc.tex --citeproc --bibliography=bibliography.yaml --csl=gost-r-7-0-5-2008-numeric-alphabetical.csl _coursework_pandoc.md
+
+# 3 раза — для корректных ссылок
+lualatex coursework.tex
+lualatex coursework.tex
 lualatex coursework.tex
